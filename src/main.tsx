2e817a3e82d57f7fd40edcb49c5af9950e4878e5
Illia Chernysh <jpdivinewind@gmail.com>
2023-02-18 15:55:21 +0200

    ReactDOM.render -> createRoot(...).render

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
-import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
 import App from './App';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </React.StrictMode>,
-    document.getElementById('root'),
-);
+const container = document.getElementById('root');
+if (container) {
+    const root = createRoot(container);
+    root.render(
+        <React.StrictMode>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </React.StrictMode>,
+    );
+} else {
+    console.error('Oops.. where is "root"?..');
+}
